3c65e50cf4721abfe911c90389bcd18856a908e9
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-22 19:40:45 +0200

    showQuestion func

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
 
-
+const shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
+    shuffledQuestions = question.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
 }
 
 function setNextQuestion() {
-
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function selectAnswer() {
