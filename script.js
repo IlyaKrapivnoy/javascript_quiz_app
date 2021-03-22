d6fda3a7be5b536650de5840c60b01c03479d0ea
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-22 19:47:26 +0200

    test

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
-const shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-    console.log('Started')
     startButton.classList.add('hide')
-    shuffledQuestions = question.sort(() => Math.random() - .5)
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
     setNextQuestion()
-}
+  }
 
 function setNextQuestion() {
     showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
 }
 
 function selectAnswer() {
@@ -26,7 +31,7 @@ const questions = [
     {
         question: 'What is 2 + 2',
         answer: [
-            { text: '4', correct: true }
+            { text: '4', correct: true },
             { text: '27', correct: false }
         ]
     }
