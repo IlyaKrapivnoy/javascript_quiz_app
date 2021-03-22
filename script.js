50dba83e1895b96246ad01c06fc2990286712682
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-22 19:37:54 +0200

    test questions

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById('question-container')
+
+
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
     console.log('Started')
     startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -14,3 +19,13 @@ function setNextQuestion() {
 function selectAnswer() {
 
 }
+
+const questions = [
+    {
+        question: 'What is 2 + 2',
+        answer: [
+            { text: '4', correct: true }
+            { text: '27', correct: false }
+        ]
+    }
+]
